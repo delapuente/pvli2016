f7713dad4cffabc3425a03a519592390fc69fdce
Carlos León <cleon@ucm.es>
2016-10-25 15:21:20 +0200

    code ready

--- tema4/components.js
+++ tema4/components.js
@@ -1,19 +1,32 @@
+// By Carlos León, 2016
+// Licensed under Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+
 'use strict';
 
 //////////////////////////////////////////////////////////////////////////////
+
+// Entity type to differentiate entities and have them attack those not
+// belonging to the same kind
 var EntityType = {
     GOOD: 0,
     EVIL: 1
 };
 
+// Entity constructor
+// 
+// Entities have a name (it doesn't have to be unique, but it helps) and a type
+//
+// Additionally, entities accept a list of instantiated components
 function Entity(entityName, entityType, components=[]) {
     var self = this;
     this.entityName = entityName;
+
+    // Instead of assigning the parameter, we call `addComponent`, which is a
+    // bit smarter than default assignment
     this.components = [];
     components.forEach(function(component) {
         self.addComponent(component);
     });
-    this.components = components;
     this.type = entityType;
 }
 
@@ -22,6 +35,9 @@ Entity.prototype.addComponent = function(component) {
     component.entity = this;
 };
 
+// This function delegates the tick on the components, gathering their messages
+// and aggregating them into a single list of messages to be delivered by the
+// message manager (the game itself in this case
 Entity.prototype.tick = function() {
     var outcoming = [];
     this.components.forEach(function(component) {
@@ -33,7 +49,10 @@ Entity.prototype.tick = function() {
     return outcoming;
 };
 
+// All received messages are forwarded to the components
 Entity.prototype.receive = function(message) {
+    // If the receiver is `null`, this is a broadcast message that must be
+    // accepted by all entities
     if(!message.receiver || message.receiver === this) {
         this.components.forEach(function(component) {
             component.receive(message);
@@ -53,6 +72,7 @@ function Component(entity=null) {
 }
 
 Component.prototype.tick = function() {
+    // We return a copy of the `messageQueue`, and we empty it
     var aux = this.messageQueue;
     this.messageQueue = [];
     return aux;
@@ -67,20 +87,32 @@ function Game(entities) {
     this.entities = entities;
     this.messageQueue = [];
 }
+
 Game.prototype.mainLoop = function (ticks=null) {
     var i = 0;
+    function line() {
+        console.log("-----------------------------------------");
+    }
     while(!ticks || i < ticks) {
+        line();
+        console.log("Tick number " + i);
+        line();
         this.tick();
         i++;
     }
 };
+
+// Each tick, all entities are notified by calling their `tick` function
 Game.prototype.tick = function () {
     var self = this;
 
+    // We create `Presence` messages for all entities to let others that they
+    // exists in the game
     this.entities.forEach(function(entity) {
         self.messageQueue.push(new Presence(entity));
     });
 
+    // All messages coming from the entities are put in the queue
     this.entities.forEach(function(entity) {
         var tickMessages = entity.tick();
 
@@ -91,6 +123,9 @@ Game.prototype.tick = function () {
 
     this.deliver();
 };
+
+
+// All messages in the queue are delivered to all the entities
 Game.prototype.deliver = function() {
     var self = this;
 
@@ -120,7 +155,7 @@ Attacker.prototype.constructor = Attacker;
 Attacker.prototype.receive = function(message) {
     if(message instanceof Presence) {
         if(message.who.type != this.entity.type) {
-            this.messageQueue.push(new Attack(message.who, this.entity));
+            this.messageQueue.push(new Attack(this.entity, message.who));
         }
     }
 };
@@ -161,10 +196,18 @@ Attack.prototype.constructor = Attack;
 
 
 
+// helper functions creating new components
 var attacker = function() { return new Attacker(); };
 var defender = function() { return new Defender(); };
-var player = new Entity("link", EntityType.GOOD, [attacker(), defender()]);
-var enemy = new Entity("ganon", EntityType.EVIL, [attacker(), defender()]);
-var game = new Game([player, enemy]);
+
+// entities in the game
+var link = new Entity("link", EntityType.GOOD, [attacker(), defender()]);
+var ganon = new Entity("ganon", EntityType.EVIL, [attacker(), defender()]);
+var octorok = new Entity("octorok", EntityType.EVIL, [defender()]);
+var armos = new Entity("armos", EntityType.EVIL, [attacker()]);
+
+// we create the game with the entities
+var game = new Game([link, ganon, armos, octorok]);
+
 game.mainLoop(10);
 
